8ce93bdc92fd5a55e986f4d1b081b48551a22c97
KingKeleos <78959857+KingKeleos@users.noreply.github.com>
2022-05-19 14:47:14 +0200

    create exe from py

--- media-sorter.py
+++ media-sorter.py
@@ -6,6 +6,15 @@ from os.path import isfile, join, isdir
 from ctypes import windll
 from tracemalloc import stop
 
+def show_exception_and_exit(exc_type, exc_value, tb):
+    import traceback
+    traceback.print_exception(exc_type, exc_value, tb)
+    input("Press key to exit.")
+    sys.exit(-1)
+
+
+sys.excepthook = show_exception_and_exit
+
 def get_drives():
     drives = []
     bitmask = windll.kernel32.GetLogicalDrives()
@@ -53,28 +62,28 @@ while not isdir(src_dir):
         if isfile(join(src_dir,f)):
             time_code=time.ctime(os.path.getmtime(os.path.join(src_dir,f)))[0:10]
             if f[-4:-1]+f[-1] in imageFormat:
-                if not isdir(sys.path[0]+"/Images"):
-                    os.mkdir(sys.path[0]+"/Images")
-                    print("created Directory for images under "+sys.path[0]+"/Images")
-                if not isdir(os.path.join(sys.path[0],"Images",time_code)):
-                    os.mkdir(os.path.join(sys.path[0],"Images",time_code))
-                if not f in os.listdir(os.path.join(sys.path[0],"Images",time_code)):
+                if not isdir(os.getcwd()+"/Images"):
+                    os.mkdir(os.getcwd()+"/Images")
+                    print("created Directory for images under "+os.getcwd()+"/Images")
+                if not isdir(os.path.join(os.getcwd(),"Images",time_code)):
+                    os.mkdir(os.path.join(os.getcwd(),"Images",time_code))
+                if not f in os.listdir(os.path.join(os.getcwd(),"Images",time_code)):
                     print("Image %s found | importing..." %f)
-                    shutil.copy2(os.path.join(src_dir,f),os.path.join(sys.path[0],"Images",time_code))
-                    print("Successfuly imported Image %s into %s" %(f,os.path.join(sys.path[0],"Images",time_code)))
+                    shutil.copy2(os.path.join(src_dir,f),os.path.join(os.getcwd(),"Images",time_code))
+                    print("Successfuly imported Image %s into %s" %(f,os.path.join(os.getcwd(),"Images",time_code)))
                     count+=1
                 else:
                     print("! - Image: %s already imported" % f)
             elif f[-4:-1]+f[-1] in videoFormat:
-                if not isdir(os.path.join(sys.path[0],"Videos")):
-                    os.mkdir(os.path.join(sys.path[0],"Videos"))
-                    print("created Directory for videos under "+sys.path[0]+"/Videos")
-                if not isdir(os.path.join(sys.path[0],"Videos",time_code)):
-                    os.mkdir(os.path.join(sys.path[0],"Videos",time_code))
-                if not f in os.listdir(os.path.join(sys.path[0],"Videos",time_code)):
+                if not isdir(os.path.join(os.getcwd(),"Videos")):
+                    os.mkdir(os.path.join(os.getcwd(),"Videos"))
+                    print("created Directory for videos under "+os.getcwd()+"/Videos")
+                if not isdir(os.path.join(os.getcwd(),"Videos",time_code)):
+                    os.mkdir(os.path.join(os.getcwd(),"Videos",time_code))
+                if not f in os.listdir(os.path.join(os.getcwd(),"Videos",time_code)):
                     print("Video %s found | importing..." %f)
-                    shutil.copy2(os.path.join(src_dir,f),os.path.join(sys.path[0],"Videos",time_code))
-                    print("Successfuly imported Video %s into %s" %(f,os.path.join(sys.path[0],"Videos",time_code)))
+                    shutil.copy2(os.path.join(src_dir,f),os.path.join(os.getcwd(),"Videos",time_code))
+                    print("Successfuly imported Video %s into %s" %(f,os.path.join(os.getcwd(),"Videos",time_code)))
                     count+=1
                 else:
                     print("! - Video %s already imported" %f)
@@ -83,3 +92,4 @@ while not isdir(src_dir):
 
 
 print("!--- Done! Copied: %d Files ---!" % count)
+input()
